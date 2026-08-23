aaa653a37b9d347c559f621180ef69d5b1a3503c
Pedro A. Rosquete Ares <rosquetearespedro06@gmail.com>
2026-08-23 17:14:25 -0400

    Multiple fixes in project structure and metadata.

--- PyInput/__init__.py
+++ PyInput/__init__.py
@@ -1,5 +1,5 @@
 """Public package interface for PyInput."""
 
-from .input import get_num, get_str, yes_no
+from .pyinput import get_num, get_str, yes_no
 
 __all__ = ["get_str", "get_num", "yes_no"]
